20a5e798dda59bbcc85781de5b5c15955a8eff17
unsignedint <rc@braveface.nz>
2020-04-07 17:43:39 +1200

    fix comments

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 STUDENT_FILE=${1:-"https://courses.edx.org/asset-v1:StanfordOnline+SOE.YCSCS1+1T2020+type@asset+block@student-dist.tar.gz"}
 ASSIGNMENT_DIR=assignments
 
-# always (re)build the container with a new dist file
+# rebuild the container with a new dist file, if it doesn't exist
 if [[ -d ".student-dist" ]]; then
   echo "The \`.student-dist\` directory already exists. Not going to download again."
 else
@@ -18,7 +18,7 @@ fi
 docker rmi -f cs143
 docker build -t cs143 .
 
-# prepare assignment workspace
+# prepare assignment workspace, if it doesn't exist
 if [[ -d "$ASSIGNMENT_DIR" ]]; then
   echo "The \`$ASSIGNMENT_DIR\` directory already exists. Not touching."
 else
