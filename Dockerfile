8e49ecfa98b506e91d3fa9be1528a194e627b0d9
unsignedint <rc@braveface.nz>
2020-04-07 17:28:13 +1200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM ubuntu:xenial
+
+COPY .student-dist /usr/class/cs143
+
+RUN apt-get update \
+    && DEBIAN_FRONTEND=noninteractive apt-get install -y libc6-i386 flex bison build-essential curl \
+    && rm -rf /var/lib/apt/lists/* \
+    && sed -i -e 's/ln -s/cp -r -L/g' /usr/class/cs143/assignments/*/Makefile \
+    && ln -s /usr/class/cs143 /root/cs143
+
+RUN curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | \
+    JABBA_COMMAND="install adopt@1.8.0-242 -o /jdk" bash
+
+ENV JAVA_HOME /jdk
+ENV PATH $JAVA_HOME/bin:$PATH
+ENV PATH /usr/class/cs143/bin:$PATH
+
+WORKDIR /usr/class/cs143/assignments
+ENTRYPOINT ["/bin/bash"]
---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+STUDENT_FILE=${1:-"https://courses.edx.org/asset-v1:StanfordOnline+SOE.YCSCS1+1T2020+type@asset+block@student-dist.tar.gz"}
+ASSIGNMENT_DIR=assignments
+
+# always (re)build the container with a new dist file
+if [[ -d ".student-dist" ]]; then
+  echo "The \`.student-dist\` directory already exists. Not going to download again."
+else
+  rm -rf .student-dist*
+  mkdir .student-dist
+  wget "$STUDENT_FILE" -O student-dist.tar.gz
+  tar xf student-dist.tar.gz -C .student-dist
+fi
+
+# docker stuff, always force a rebuild
+docker rmi -f cs143
+docker build -t cs143 .
+
+# prepare assignment workspace
+if [[ -d "$ASSIGNMENT_DIR" ]]; then
+  echo "The \`$ASSIGNMENT_DIR\` directory already exists. Not touching."
+else
+  cp -r .student-dist/assignments $ASSIGNMENT_DIR
+fi
